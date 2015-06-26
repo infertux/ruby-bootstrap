350ed83ffb96a8a88d04b2d4333904a6719e15e7
Cédric Félizard <cedric@felizard.fr>
2015-06-26 16:53:49 +0000

    Fix wrong URL

--- bootstrap_ruby_2.2.sh
+++ bootstrap_ruby_2.2.sh
@@ -30,7 +30,7 @@ command -v ruby >/dev/null || {
   fi
 
   cd /tmp
-  wget http://cache.ruby-lang.org/pub/ruby/2.1/ruby-${RUBY}.tar.gz
+  wget http://cache.ruby-lang.org/pub/ruby/2.2/ruby-${RUBY}.tar.gz
   echo "${SHA256}  ruby-${RUBY}.tar.gz" | sha256sum -c -
   tar --no-same-owner -xf ruby-${RUBY}.tar.gz
 
