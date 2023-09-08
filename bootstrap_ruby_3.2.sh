c6f14d3adace0077c6ddfa59872ba430dc524e7b
Cédric Félizard <cedric@felizard.fr>
2023-09-08 16:11:09 +0000

    Add missing libyaml dependency for Ruby 3.2

--- bootstrap_ruby_3.2.sh
+++ bootstrap_ruby_3.2.sh
@@ -20,9 +20,9 @@ if [ -n "$FORCE" ] || ! command -v ruby >/dev/null; then
 
   if [ -f /etc/debian_version ]; then
     apt-get update
-    apt-get install -y wget gcc make zlib1g-dev libssl-dev libreadline-dev libffi-dev
+    apt-get install -y wget gcc make libffi-dev libreadline-dev libssl-dev libyaml-dev zlib1g-dev
   elif [ -f /etc/redhat-release ]; then
-    yum install -y wget gcc make zlib-devel openssl-devel readline-devel
+    yum install -y wget gcc make openssl-devel readline-devel zlib-devel
   fi
 
   cd /tmp
